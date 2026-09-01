abcf94e5b40fe877acadf2c1ce821aa961d302a4
Giovani-Sella <giovanisellajor@gmail.com>
2026-09-01 14:20:38 -0300

    rodape

--- componentes/rodape.js
+++ componentes/rodape.js
@@ -12,7 +12,10 @@ function injetarRodape(elementId) {
     <p class="textoRodape2">
       Projeto mantido e criado por
       <a href="https://www.giovanisella.com.br" target="_blank">Giovani Sella</a>. 
-    </p><a href="https://github.com/Giovani-Sella/Como-vota-Curitiba" target="_blank">Ver no GitHub</a>.
+    </p>
+    <p class="textoRodape2">
+      <a href="https://github.com/Giovani-Sella/Como-vota-Curitiba" target="_blank">Ver no GitHub</a>
+    </p>
     <p class="textoRodape2">© ${ano} Como Vota Curitiba — Todos os direitos reservados</p>
   `;
 }
